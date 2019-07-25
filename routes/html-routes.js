49ba1bfad56546350cda3ab4b9b5c5461da5d2de
Dameon Brown <axel12gt@live.com>
2019-07-25 17:35:28 -0400

    cleaned up html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,12 +3,11 @@ module.exports = app => {
     
 
 app.get("/", (req,res) => {
-    // Grab every document in the Articles collection
-    db.Article.find({})
+    
     // If Articles are found send them to the client
-    .then(dbArticle => res.render("index"))
+    res.render("index")
     // If error happens send it back to the client
-    .catch(err => res.json(err))
+    
 })
 
 }
